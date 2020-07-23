3e13e2ebd84053d3feca00abd8c411438f4e8926
Lenovo <Lenovo@DESKTOP-K4DFE7A>
2020-07-23 14:45:35 +0530

    Master v1.0
    Finally Working in NetBeans IDE

---
+++ src/main/java/com/harsh/practice/HomeController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.harsh.practice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String display() {
+        return "home";
+    }
+    @RequestMapping("/second")
+    public String display2() {
+        return "second";
+    }
+}
